c79c2d59d0186661fb73ae026668ad72a1d75dfd
Rin0530 <onochanbasket@gmail.com>
2022-05-05 14:46:16 +0900

    installスクリプト編集(要検証)

--- initForMac.sh
+++ initForMac.sh
@@ -1,21 +1,25 @@
-#! /bin/sh
 brew install deno
 ret=$?
-rc=~/.profile
 if test $ret -ne 0 ; then
     curl -fsSL https://deno.land/x/install/install.sh | sh
-    if [ -f ~/.bashrc ] ; then
-        rc=~/.bashrc
-    elif [ -f ~/.zshrc ] ; then
-        rc=~/.zshrc
-    elif ! [ -f ~/.profile ] ; then
-        echo "\e[31merror\e[m" 1>&2
-        exit
-    fi
-    echo export DENO_INSTALL="$HOME/.deno" >> $rc
-    echo export PATH="$HOME/.deno/bin:$PATH" >> $rc
+    deno_path=$HOME/.deno/bin/deno
+else 
+    deno_path=$(which deno)
 fi
-$HOME/.deno/bin/deno install -qAn vr https://deno.land/x/velociraptor@1.0.1/cli.ts
+
+
+if test $SHELL = "/bin/bash" ; then
+    rc=~/.bashrc
+elif test $SHELL = "/bin/zsh" ; then
+    rc=~/.zshrc
+else 
+    rc=~/.profile
+fi
+
+echo export DENO_INSTALL="$HOME/.deno" >> $rc
+echo export PATH="$HOME/.deno/bin:$PATH" >> $rc
+
+$deno_path install -qAn vr https://deno.land/x/velociraptor@1.0.1/cli.ts > /dev/null 2>&1
 echo BOT_TOKEN="REPLACE YOUR TOKEN" >> env
 echo CLIENT_ID="REPLACE YOUR ID" >> env
 
