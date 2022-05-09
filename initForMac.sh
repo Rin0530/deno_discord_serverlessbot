a3a1352e092b5c64d54a3e82ff416433a897b7d1
Rin0530 <onochanbasket@gmail.com>
2022-05-09 18:03:06 +0900

    vrのインストールバージョンを最新に変更

--- initForMac.sh
+++ initForMac.sh
@@ -19,7 +19,7 @@ fi
 echo export DENO_INSTALL="$HOME/.deno" >> $rc
 echo export PATH="$HOME/.deno/bin:$PATH" >> $rc
 
-$deno_path install -qAn vr https://deno.land/x/velociraptor@1.0.1/cli.ts > /dev/null 2>&1
+$deno_path install -qAn vr https://deno.land/x/velociraptor@1.5.0/cli.ts > /dev/null 2>&1
 echo BOT_TOKEN="REPLACE YOUR TOKEN" >> env
 echo Application_ID="REPLACE YOUR ID" >> env
 
