0f820b3af33fd11d5eb473d8af73097f8e0e6af4
Rin0530 <onochanbasket@gmail.com>
2022-05-10 23:05:08 +0900

    テスト用の例外削除

--- src/commands/hello.ts
+++ src/commands/hello.ts
@@ -2,7 +2,6 @@ import { ApplicationCommand, InteractionResponse } from "../../@types/index.d.ts
 import { InteractionCallbackType } from "../../@types/types.ts"
 
 export function hello(data:ApplicationCommand):InteractionResponse {
-    throw Error()
     // オプションを取得
     const value = data.options?.find(predicate => predicate.name == "name")?.value;
     const res: InteractionResponse = {
