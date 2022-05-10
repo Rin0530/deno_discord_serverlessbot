ccd34c7d7537db113c61afda28c91e8644d3b144
Rin0530 <onochanbasket@gmail.com>
2022-05-10 23:06:21 +0900

    optionのnameを取得するコードを少し短くした

--- src/commands/hello.ts
+++ src/commands/hello.ts
@@ -3,13 +3,13 @@ import { InteractionCallbackType } from "../../@types/types.ts"
 
 export function hello(data:ApplicationCommand):InteractionResponse {
     // オプションを取得
-    const value = data.options?.find(predicate => predicate.name == "name")?.value;
+    const name = data.options?.find(val => val.name == "name")?.value;
     const res: InteractionResponse = {
         // Type 4 reponds with the below message retaining the user's
         // input at the top.
         type: InteractionCallbackType.CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
-          content: `Hello, ${value}!`,
+          content: `Hello, ${name}!`,
         },
       }
     return res;
