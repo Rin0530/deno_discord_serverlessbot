9ca64ec26e593b3329aef24936100fa0276eead4
Rin0530 <onochanbasket@gmail.com>
2022-05-12 21:59:45 +0900

    コマンド登録までをスクリプトで行うように変更

--- initForMac.sh
+++ initForMac.sh
@@ -1,7 +1,11 @@
-brew install deno
+#! /bin/bash
+read -s -p "BOT_TOKENを入力してください: " TOKEN
+read -s -p "Application_IDを入力してください: " 18 ID
+
+brew install deno > /dev/null 2>&1
 ret=$?
 if test $ret -ne 0 ; then
-    curl -fsSL https://deno.land/x/install/install.sh | sh
+    curl -fsSL https://deno.land/x/install/install.sh | sh > /dev/null 2>&1
     deno_path=$HOME/.deno/bin/deno
 else 
     deno_path=$(which deno)
@@ -16,11 +20,28 @@ else
     rc=~/.profile
 fi
 
+export DENO_INSTALL="$HOME/.deno"
+export PATH="$HOME/.deno/bin:$PATH"
+
 echo export DENO_INSTALL="$HOME/.deno" >> $rc
 echo export PATH="$HOME/.deno/bin:$PATH" >> $rc
 
 $deno_path install -qAn vr https://deno.land/x/velociraptor@1.5.0/cli.ts > /dev/null 2>&1
-echo BOT_TOKEN="REPLACE YOUR TOKEN" >> env
-echo Application_ID="REPLACE YOUR ID" >> env
+
+
+
+echo BOT_TOKEN=$TOKEN >> env
+echo Application_ID=$ID >> env
+
+# setting.jsonも生成したい
+mkdir .vscode
+cat << EOF > .vscode/setting.json
+{
+    "deno.enable": true,
+    "deno.path": "$DENO_INSTALL/bin"
+}
+EOF
+
+$HOME/.deno/bin/vr create
 
 echo "please execution 'source $rc'"
--- initForWin.bat
+++ initForWin.bat
@@ -1,7 +1,13 @@
+@echo off
+echo "BOT_TOKEN‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢ > "
+SET /P BOT_TOKEN=
+echo "Application_ID‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢ > "
+SET /P Application_ID=
 powershell -NoProfile -ExecutionPolicy Unrestricted -Command "& { iwr https://deno.land/x/install/install.ps1 -useb | iex }"
 %HOMEPATH%\.deno\bin\deno install -qAn vr https://deno.land/x/velociraptor@1.0.1/cli.ts
 if %errorlevel%==0 (
-    echo BOT_TOKEN="REPLACE YOUR TOKEN" >> env
-    echo Application_ID="REPLACE YOUR ID" >> env
+    echo BOT_TOKEN= %BOT_TOKEN% >> env
+    echo Application_ID=%Application_ID% >> env
+    %HOMEPATH%\.deno\bin\vr create
 )
 else pause
