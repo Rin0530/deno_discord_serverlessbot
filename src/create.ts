1c0b31a64930af629b078ced5930b3578cc21338
Rin0530 <onochanbasket@gmail.com>
2022-05-19 00:12:08 +0900

    仮コマンド削除

--- src/create.ts
+++ src/create.ts
@@ -4,7 +4,7 @@ import { GlobalApplicationCommand } from "../@types/index.d.ts"
 const BOT_TOKEN = ENV.require("BOT_TOKEN")
 const Application_ID = ENV.require("Application_ID")
 
-const url = `https://discord.com/api/v8/applications/${Application_ID}/commands`;
+const url = `https://discord.com/api/v8/applications/${Application_ID}/commands/976501341843238922`;
 
 const headers = {
     Authorization: `Bot ${BOT_TOKEN}`
@@ -16,7 +16,8 @@ const body:GlobalApplicationCommand = {
 }
 
 try {
-    await KY.post(url, {headers:headers, json: body }).json()
+    //await KY.post(url, {headers:headers, json: body }).json()
+    await KY.delete(url).json()
 } catch (e) {
     console.log("error");
     console.error(e)
