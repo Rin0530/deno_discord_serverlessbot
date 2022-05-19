2a47b257b8312a30fc4387e85e2221378a4d6b43
Rin0530 <onochanbasket@gmail.com>
2022-05-19 09:40:10 +0900

    velociraptorのバージョン変更

--- initForWin.bat
+++ initForWin.bat
@@ -7,7 +7,7 @@ SET /P Application_ID=
 echo 必要なもののダウンロードを開始します
 
 powershell -NoProfile -ExecutionPolicy Unrestricted -Command "& { iwr https://deno.land/x/install/install.ps1 -useb | iex }"
-%HOMEPATH%\.deno\bin\deno install -qAn vr https://deno.land/x/velociraptor@1.0.1/cli.ts
+%HOMEPATH%\.deno\bin\deno install -qAn vr https://deno.land/x/velociraptor@1.5.0/cli.ts
 if %errorlevel%==0 (
     echo BOT_TOKEN= %BOT_TOKEN% >> env
     echo Application_ID=%Application_ID% >> env
